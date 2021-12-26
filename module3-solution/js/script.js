d0803d562ca74ad9312623741438553e45bd8ccc
Harold Bustos <haroldbustos@Harolds-MacBook-Air.local>
2021-12-25 20:41:16 -0700

    update

---
+++ module3-solution/js/script.js
@@ -0,0 +1,8 @@
+console.log("Start");
+console.log(Boolean(console.log("Hello")));
+console.log("Finish");
+
+var x = 10;
+if ( (null) || (console.log("Hello")) || x > 5 ) {
+	console.log("Hello");
+}
